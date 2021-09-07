1f81981c94855bdd722d8ff8c11d7608c7afa4b6
joshua <jshuaang@gmail.com>
2021-09-08 00:09:07 +0700

    disable eslint

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["image.tmdb.org"]
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 }
