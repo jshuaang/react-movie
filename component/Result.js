20c99592d8a0a8675ce91ba596263e3b1553a7f4
joshua <jshuaang@gmail.com>
2021-09-21 19:55:16 +0700

    fix search result not found

--- component/Result.js
+++ component/Result.js
@@ -4,13 +4,24 @@ import {useRouter} from 'next/router'
 function Result({results}) {
     const router = useRouter()
 
-    return (
-        <div className="h-full w-full px-5 my-5 sm:grid sm:grid-cols-3">
-            {results.map( res => (
-                <Thumbnail key={res.id}  result={res} onClick={() => router.push(`/detail?id=${res.id}`, console.log(res))}/>
-            ))}
-        </div>
-    )
+    console.log(results.length)
+    if(results.length > 0){
+        return (
+            <div className="h-full w-full px-5 my-5 sm:grid sm:grid-cols-3">
+                {results.map( res => (
+                    <Thumbnail key={res.id}  result={res} onClick={() => router.push(`/detail?id=${res.id}`, console.log(res))}/>
+                ))}
+            </div>
+        )
+    }else{
+        return (
+            <div className="text-center font-extrabold text-2xl">
+                <h1>{router.query.value} not found</h1>
+            </div>
+        )
+    }
+    
+
 }
 
 export default Result
